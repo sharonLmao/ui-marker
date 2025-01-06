e6c06ee23ef7bfe85d3fc432926f5d281a8d6f05
Burgil‬‏ <41600149+burgil@users.noreply.github.com>
2025-01-06 23:12:23 +0200

    improved styles

--- client.lua
+++ client.lua
@@ -1,10 +1,19 @@
 function PrintDistanceToTarget(distance)
     if ShouldUseMetricMeasurements() then
-        local distanceInKm = distance / 1000
-        return string.format("%.5f km.", distanceInKm)
+        if distance >= 1000 then
+            local distanceInKm = distance / 1000
+            return string.format("%.2f km", distanceInKm)
+        else
+            return string.format("%d m", math.floor(distance))
+        end
     else
-        local distanceInMiles = distance / 1609.34
-        return string.format("%.0f mi.", distanceInMiles)
+        if distance >= 1609.34 then
+            local distanceInMiles = distance / 1609.34
+            return string.format("%.2f mi", distanceInMiles)
+        else
+            local distanceInFeet = distance * 3.28084
+            return string.format("%d ft", math.floor(distanceInFeet))
+        end
     end
 end
 
