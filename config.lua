40f602edde2599d63bea5ddf65eb2c76c2d88891
Sharon Shamay <84649681+sharonLmao@users.noreply.github.com>
2025-01-07 07:22:00 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -2,17 +2,17 @@ Config = {}
 
 Config.debug = false
 
-Config.UPDATE_SPEED = 0.6
+Config.UPDATE_SPEED = 4.8
 Config.DELAY_SPEED = 1500
-Config.GLOW_DISTANCE = 500.0
+Config.GLOW_DISTANCE = 70.0
 
 Config.targetCoords = {
     ["home"] = {
-        coords = vector3(2829.993896, 1474.732544, 24.555395),
-        label = "Home",
-        distance = 10.0,
-        show = false,
-        glow_obj = nil
+        coords = vector3(2829.993896, 1474.732544, 24.555395), -- waypoint coords
+        label = "Home", -- label text
+        distance = 10.0, -- label distance
+        show = false, -- show on load
+        glow_obj = nil -- object to glow
     },
     ["carrierdoor"] = {
         coords = vector3(598.093079, -3416.244873, 6.077423),
@@ -26,7 +26,7 @@ Config.targetCoords = {
         label = "Carrier bridge",
         distance = 5.0,
         show = false,
-        glow_obj = `po1_03_ramp_011`
+        glow_obj = `po1_03_ramp011`
     },
     ["test2"] = {
         coords = vector3(2123.456789, 1789.012345, 32.345678),
